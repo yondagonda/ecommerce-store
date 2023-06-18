4219807f3283a927c763da6274be830f2d5dac23
Johny <yonniedooo@gmail.com>
2023-06-19 06:52:51 +1000

    adjustments to media query

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,7 +21,7 @@ export const Home = () => {
   return (
     <motion.div
       className="text-slate-50 flex flex-col items-center gap-3 mt-10 sm:mt-16 sm:gap-6
-    lg:mt-[170px] 2xl:mt-[220px] 3xl:mt-[260px] lg:flex-row lg:justify-evenly"
+    lg:mt-[170px] 2xl:mt-[200px] 3xl:mt-[260px] lg:flex-row lg:justify-evenly"
       onLoad={() => setIsBackgroundVisible(true)}
       initial={{ opacity: 0, y: -20 }}
       animate={{
@@ -45,14 +45,14 @@ export const Home = () => {
             duration: 0.12,
           }}
           className="text-center bg-white/20 p-4 rounded-xl backdrop-blur-[4px] w-[94%]
-      xs:w-[400px] sm:w-[550px] lg:w-[650px] 2xl:w-[800px] 3xl:w-[900px] 
-      xs:p-6 sm:p-8 2xl:p-12 3xl:p-20 cursor-default"
+      xs:w-[400px] sm:w-[550px] lg:w-[650px] 2xl:w-[750px] 3xl:w-[900px] 
+      xs:p-6 sm:p-8 2xl:p-10 3xl:p-16 cursor-default"
         >
           {/* ADD IN SIZE XL MEDIA QUERIES LATER  */}
-          <div className="text-4xl font-bold sm:text-6xl lg:text-8xl 2xl:text-[120px] 3xl:text-[140px] select-none">
+          <div className="text-4xl font-bold sm:text-6xl lg:text-8xl 2xl:text-[110px] 3xl:text-[140px] select-none">
             Game Store
           </div>
-          <div className="text-base sm:text-lg lg:text-xl 2xl:text-2xl select-none">
+          <div className="text-base sm:text-lg lg:text-xl 3xl:text-2xl select-none">
             The store where your gaming dreams come true! Discover a curated
             collection of the latest releases, timeless classics, and hidden
             gems. Immerse yourself in captivating adventures, intense battles,
@@ -105,7 +105,7 @@ export const Home = () => {
         className="bg-white/20 p-4 rounded-xl text-center backdrop-blur-[2px] flex flex-col gap-1 
       items-center w-[300px] sm:w-[340px] xl:w-[400px] 2xl:w-[440px] 3xl:w-[500px] lg:py-10 mb-4 select-none"
       >
-        <div className="text-2xl font-bold pb-2 lg:text-3xl 2xl:text-4xl cursor-default 3xl:text-5xl select-none">
+        <div className="text-2xl font-bold pb-2 lg:text-3xl cursor-default 3xl:text-4xl select-none">
           Quick Navigation
         </div>
 
